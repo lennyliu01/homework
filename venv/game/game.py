e8e31fc49d7d99800d1a54076af0a12a816dfa11
Lenny Liu <cong.liu@oyohotels.co.jp>
2020-06-13 23:53:31 +0900

    inital work on chopstciks

--- venv/game/game.py
+++ venv/game/game.py
@@ -50,4 +50,38 @@ class SubtractSquare(Game):
             return True
         if player == "Player 2" and not self.is_player1_first:
             return False
+
+class Chopsticks(Game):
+    def __init__(self, is_player1_first:bool):
+        self.is_player1_first = is_player1_first
+        self.state = { 'Player 1': {'right_hand': 1,'left_hand':1},'Player 2': {'right_hand': 1, 'left_hand':1}}
+    
+    def oppsite_player(self,player):
+        if player == 'Player 1':
+            return 'Player 2'
+        else:
+            return 'Player 1'
+    
+    def get_possible_moves(self,player):
+        player_hands  = []
+        oppsite_hands = []
+        for k,v in self.state[player].items():
+            if v < 5:
+                player_hands.append(k)
+        for k,v in self.state[self.oppsite_player(player)].items():
+            if v < 5:
+                oppsite_hands.append(k)
+        moves = {}
+        n=1
+        for i in range(len(player_hands)):
+            for j in range(len(oppsite_hands)):
+                moves.update({n: player + ' ' + player_hands[i] + ' touches ' + self.oppsite_player(player) + ' ' + oppsite_hands[j]})
+                n += 1
+        return moves
+    
+    def make_move(self, move):
+        pass
+            
+   
+    
        
